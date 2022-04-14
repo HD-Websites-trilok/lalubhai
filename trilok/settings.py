196b8389cd27dc485569a522d043d001ccbe2426
unknown <dhansukhharkhani@gmail.com>
2022-04-14 14:34:37 +0530

    hi

--- trilok/settings.py
+++ trilok/settings.py
@@ -15,7 +15,7 @@ import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -42,7 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'whitenoise.runserver_nostatic',
     'crispy_forms',
-    'stockmgmt' 
+    'custom_user',
+    'stockmgmt', 
 ]
 
 MIDDLEWARE = [
